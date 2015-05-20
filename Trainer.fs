c7157cef8be36aed90269da1f041b472776ee4e1
John Donnellan <john@blueTribu.com>
2015-05-20 09:03:06 +0100

    wip! added network, feedforward and propagateback, working on trainer

---
+++ Trainer.fs
@@ -0,0 +1,15 @@
+﻿module Trainer
+
+open Network
+open Sample
+//let private checkDimensions net cases =
+//    if Array.length cases <> 0 then
+//        ////////////
+
+let TrainAllCases net samples =
+    cases 
+    |> Array.iter(fun sample ->
+        feedForward net sample.Input |> ignore
+     )
+    ()
+
